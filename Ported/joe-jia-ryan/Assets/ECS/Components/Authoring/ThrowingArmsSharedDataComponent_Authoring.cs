281e295e3be1612749b572ab1877c55cb2aca67d
Joe Jones <joejo@unity3d.com>
2019-10-29 16:36:19 -0700

    Add shared components, add authoring converter for shared state component, apply to new empty game entity.

---
+++ Ported/joe-jia-ryan/Assets/ECS/Components/Authoring/ThrowingArmsSharedDataComponent_Authoring.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+using Unity.Entities;
+
+public class ThrowingArmsSharedDataComponent_Authoring : MonoBehaviour, IConvertGameObjectToEntity
+{
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        var data = new ThrowingArmsSharedDataComponent{};
+        dstManager.SetSharedComponentData(entity, data);
+    }
+}
