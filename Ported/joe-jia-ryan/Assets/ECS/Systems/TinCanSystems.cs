29dbf229bcaa3957d83c291b0021f4d827e3e135
Jia XU <jia@unity3d.com>
2019-10-29 17:11:00 -0700

    Add: Tin Can Spawn Authoring Component Add: Tin Can Systems

---
+++ Ported/joe-jia-ryan/Assets/ECS/Systems/TinCanSystems.cs
@@ -0,0 +1,48 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using static Unity.Mathematics.math;
+using UnityEngine;
+
+public struct TinCanSpawnComponent : IComponentData
+{
+    public Entity TinCanPrefab;
+    public Vector3 Size;
+    public float Gravity;
+    public int TotalCount;
+}
+
+[UpdateInGroup(typeof(SimulationSystemGroup))]
+public class TinCanSpawnSystem : JobComponentSystem
+{
+    BeginInitializationEntityCommandBufferSystem m_EntityCommandBufferSystem;
+
+    protected override void OnCreate()
+    {
+        base.OnCreate();
+        m_EntityCommandBufferSystem = World.GetOrCreateSystem<BeginInitializationEntityCommandBufferSystem>();
+    }
+
+    [BurstCompile]
+    struct TinCanSpawnJob : IJobForEachWithEntity<TinCanSpawnComponent, LocalToWorld>
+    {
+        public EntityCommandBuffer.Concurrent CommandBuffer;        
+        
+        public void Execute(Entity entity, int index, [ReadOnly] ref TinCanSpawnComponent spawnComponent, [ReadOnly] ref LocalToWorld location)
+        {
+            for (int i = 0; i < spawnComponent.TotalCount; i++)
+            {
+
+            }
+        }
+    }
+    
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        var job = new TinCanSpawnJob();
+        return job.Schedule(this, inputDeps);
+    }
+}
