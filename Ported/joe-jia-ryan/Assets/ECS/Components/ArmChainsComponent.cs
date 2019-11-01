78035e551daa7bf09a995137b5129e74813bd0ac
Ryan Jones <ryanjo@RYANJONES-MBP.local>
2019-11-01 13:04:37 -0700

    add missing file

---
+++ Ported/joe-jia-ryan/Assets/ECS/Components/ArmChainsComponent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+
+[InternalBufferCapacity(3)]
+public struct ArmChainsBuffer : IBufferElementData
+{
+    public float3 element;
+}
+
+[InternalBufferCapacity(16)]
+public struct FingerChainsBuffer : IBufferElementData
+{
+    public float3 element;
+}
+
+[InternalBufferCapacity(4)]
+public struct ThumbChainsBuffer : IBufferElementData
+{
+    public float3 element;
+}
